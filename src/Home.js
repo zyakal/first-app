ef9a7807a9d4ffa0f7cd6f07c85369cab6601cb3
zyakal <pkd1426@gmail.com>
2022-08-24 16:18:13 +0900

    08.24

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <div>
+        <Link to="/todo">Todo</Link>
+      </div>
+      <div>
+        <Link to="/movie">movie</Link>
+      </div>
+      <div>
+        <Link to="/coin">coin</Link>
+      </div>
     </div>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,17 +1,25 @@
-export default function movie({ data }) {
+// MovieApp 에서 data라는 이름으로 보내준 데이터를 기반으로 반복문
+export default function Movie({ data }) {
+  const { title, url, medium_cover_image, summary, genres } = data; // 구조 분할
   return (
-    <div key={data.id}>
-      <h2>
-        <a href="">{data.title}</a>
-      </h2>
-      <img src={data.medium_cover_image} alt={data.title} />
-      <p>{data.summary}</p>
-
-      <ul>
-        {data.genres.map((item, idx) => (
-          <li key={idx}>{item}</li>
-        ))}
+    <li key={data.id}>
+      <div>
+        <h2>
+          <a href={url}>{title}</a>
+        </h2>
+        <img src={medium_cover_image} alt={title} />
+      </div>
+      <ul className="genresList">
+        {genres.map(
+          (
+            item,
+            idx // item == genres의 요소들 , idx == 요소들을 반복문 돌릴 기준
+          ) => (
+            <li key={idx}>{item}</li>
+          )
+        )}
       </ul>
-    </div>
+      <p className="summary">{summary}</p>
+    </li>
   );
 }
